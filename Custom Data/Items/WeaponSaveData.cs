88b3882adc6bda90291c93c5f2ed5f0f511f4007
shawn.pelley <shawn.pelley@verbinteractive.co>
2021-07-30 09:23:44 -0300

    Updated WeaponSaveData to inherit properly

--- Custom Data/Items/WeaponSaveData.cs
+++ Custom Data/Items/WeaponSaveData.cs
@@ -3,17 +3,8 @@ using System.Collections;
 using System.Collections.Generic;
 
 [Serializable]
-public class WeaponSaveData : ISaveData
+public class WeaponSaveData : EquipmentSaveData
 {
-    public string id;
-    public string ID { get { return id; } }
-    public string loadType;
-    public string LoadType { get { return loadType; } }
-    public string name;
-    public string description;
-    public int equipmentSlot;
-    public List<StatBonus> statBonuses;
-    public string[] statusEffectIDs;
     public string weaponSkillID;
     public int weaponAttack;
     public bool twoHanded;
